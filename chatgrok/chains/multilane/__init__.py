63d1f79124df0cd282f918f72f269f1d816fc9d9
German Greiner <ggreiner9@gmail.com>
2023-04-16 15:03:04 +0200

    added threading

--- chatgrok/chains/multilane/__init__.py
+++ chatgrok/chains/multilane/__init__.py
@@ -1,10 +1,12 @@
 import os
 import logging
+import threading
 from .worker import GPTWorker
 from .grounding import GPTGrounding
 from .prompt_loader import PromptLoader
 
 logger = logging.getLogger('chatgrok.multilane')
+
 
 class Multilane:
 
@@ -17,6 +19,24 @@ class Multilane:
         self._grounding = GPTGrounding(os.environ['GPT_MODEL'], prompt_loader)
 
     def call(self, query):
-        worker_outputs = [worker.ask(query) for worker in self._workers]
+        threads = []
+        worker_outputs = []
+
+        # Start threads
+        for index, worker in enumerate(self._workers):
+            logger.debug("Starting worker thread [%d]", index)
+
+            def worker_thread_function():
+                return worker_outputs.append(worker.ask(query))
+            worker_thread = threading.Thread(target=worker_thread_function)
+            threads.append(worker_thread)
+            worker_thread.start()
+
+        # Join threads
+        for index, thread in enumerate(threads):
+            logger.debug("Joining worker thread [%d]", index)
+            thread.join()
+            logger.info("Worker thread [%d] is done", index)
+
         logger.debug(f"Grounding...")
         return self._grounding.ask(query, worker_outputs)
